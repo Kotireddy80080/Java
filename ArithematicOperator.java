af1c276a2f59987c9705bc072f56f5b45f3bea02
sanivarapu koti reddy <kotireddy@gmail.com>
2025-03-09 08:50:39 +0530

    arithematic operator

---
+++ ArithematicOperator.java
@@ -0,0 +1,62 @@
+class ArithematicOperator
+{
+    public static void main(String[] args)
+    {
+        int num1=20;
+        int num2=2;
+        int result1=num1+num2;
+        System.out.println(result1);
+
+        int num3=20;
+        int num4=2;
+        int result2=num3-num4;
+        System.out.println(result2);
+
+        int num5=20;
+        int num6=2;
+        int result3=num5*num6;
+        System.out.println(result3);
+
+        int num7=20;
+        int num8=2;
+        int result4=num7/num8;
+        System.out.println(result4);
+
+        int num9=20;
+        int num10=2;
+        int result5=num9%num10;
+        System.out.println(result5);
+
+        int num11=7;
+        int num12 =num11+2;
+        System.out.println(num12);
+
+        int n=7;
+        int a =n-2;
+        System.out.println(a);
+
+        int b=7;
+        b+=2;
+        System.out.println(b);
+
+        int c=7;
+        c-=2;
+        System.out.println(c);
+        
+        int d=7;
+        d++;
+        System.out.println(d);  //post inc
+
+        int e=7;
+        e--;
+        System.out.println(e);  //post dec
+
+        int f=7;
+        ++f;
+        System.out.println(f);  //pree inc
+
+        int g=7;
+        --g;
+        System.out.println(g);   //pree dec
+    }
+}
