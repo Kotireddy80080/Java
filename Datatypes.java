5c37862e168418b1342dda0663166f7851295114
sanivarapu koti reddy <kotireddy@gmail.com>
2025-03-09 08:38:54 +0530

    datatypes

---
+++ Datatypes.java
@@ -0,0 +1,17 @@
+class Datatypes
+{
+    public static void main(String[] args)
+    {
+        int num=9;  //-2147483648 t0 2147483647
+        byte by=127;  //-128 to 127
+        short sh=558;  //-559 to 558
+        long l=3776l;
+
+        float f=5.6f;
+        double d=5.8;
+
+        char c='k';
+
+        boolean b=true;
+    }
+}
