f2650ed2810aaa72788c60a5ab3275a07af59a37
sanivarapu koti reddy <kotireddy@gmail.com>
2025-03-09 08:32:28 +0530

    variables

---
+++ Variable1.java
@@ -0,0 +1,10 @@
+class Variable1
+{
+    public static void main(String[] args)
+    {
+        int num1=3;
+        int num2=8;
+        int result1=num1+num2;
+        System.out.println(result1);
+    }
+}
