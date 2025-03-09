379c2e3e3eae5d25e7f13f2dec7f36c76cd5d4da
sanivarapu koti reddy <kotireddy@gmail.com>
2025-03-09 08:48:24 +0530

    typecasting

---
+++ TypeCasting.java
@@ -0,0 +1,18 @@
+class TypeCasting
+{
+    public static void main(String[] args)
+    {
+        int a=257;
+        byte k=(byte)a;
+      System.out.println(k);
+
+      int x=32768;
+      byte y=(byte) x;
+      System.out.println(y);
+
+      byte c=10;
+      byte b=30;
+      int result1=c*b;
+      System.out.println(result1);
+    }
+}
