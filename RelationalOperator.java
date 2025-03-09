fce02105a4ec8f78b1c9479e776531b15833a53a
sanivarapu koti reddy <kotireddy@gmail.com>
2025-03-09 08:52:22 +0530

    RelationalOperator

---
+++ RelationalOperator.java
@@ -0,0 +1,30 @@
+class RelationalOperator
+{
+    public static void main(String[] args)
+    {
+        int a=6;
+        int b=7;
+        boolean c=a>b;
+        System.out.println(c);
+
+        int d=6;
+        int e=7;
+        boolean f=d<e;
+        System.out.println(f);
+
+        int a1=6;
+        int b2=7;
+        boolean c1=a1==b2;
+        System.out.println(c1);
+
+        int a3=6;
+        int b4=7;
+        boolean c2=a3>=b4;
+        System.out.println(c2);
+
+        int a5=6;
+        int b6=7;
+        boolean c3=a5!=b6;
+        System.out.println(c3);
+    }
+}
